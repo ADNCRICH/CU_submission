a4618ea5b5664b69bb8f961da7af885471ef6e64
ADNCRICH <p50629.2013x@gmail.com>
2021-10-07 13:21:16 +0700

    initial

--- Python/0.py
+++ Python/0.py
@@ -111,7 +111,7 @@ for i in range(0, len(key)-3):
 a = "e r"
 aa = a.strip().split()
 print('0') if len(aa) == 0 else print(" ".join(aa))
-"""
+
 
 
 def p(a, b):
@@ -120,3 +120,72 @@ def p(a, b):
 
 a, b, c = p(2, 3)
 print(a, b, c)
+
+a = [i.strip() for i in input().split(',')]
+print('\n'.join(a))
+
+a = [5, 6, 5, 462, 3321, 5, 6523, 32321, 3542, 13, 32132, 3]
+aa = [[i, j] for i, j in enumerate(a)]
+aa = sorted(aa, key=lambda x: x[1])
+for i in aa:
+    print(i[0], i[1])
+
+a = input()
+n = int(input())
+print(a[(n-1)*5:(n*5)])
+print(a[0:(n-1)*5]+a[(n*5):])
+
+a = input().split()
+s = b = p = 0
+for i in a:
+    if i == 'shirt':
+        s += 1
+    elif i == 'pants':
+        p += 1
+    elif i == 'bag':
+        b += 1
+if s >= 2:
+    p = max(0, p-1)
+if s >= 3:
+    b = max(0, b-1)
+print(200*(s+b+p))
+
+a = [float(i) for i in input().split()]
+n = min(int(a[0]), len(a)-1)
+a = a[1:]
+for i in range(len(a)):
+    print(max(a[i:min(i+n, len(a))]), end=" ")
+
+a = [['A102', 30], ['A103', 40], ['B014', 50], ['B109', 50]]
+x, y = [int(i) for i in input().split(',')]
+ans = []
+for i in a:
+    if i[1] < x or i[1] > y:
+        ans.append(i)
+print(ans)
+
+a = [85, 80, 75, 70, 65, 60, 55, 50, 45, 40]
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.DataFrame({"A": ["foo", "foo", "foo", "foo", "foo",
+                         "bar", "bar", "bar", "bar"],
+                   "B": ["one", "one", "one", "two", "two",
+                         "one", "one", "two", "two"],
+                   "C": ["small", "large", "large", "small",
+                         "small", "large", "small", "small",
+                         "large"],
+                   "D": [1, 2, 2, 3, 3, 4, 5, 6, 7],
+                   "E": [2, 4, 5, 5, 6, 6, 8, 9, 9]})
+df
+"""
+real = "efFF"
+fake = "zsrgzsrg"
+a = [[0 for i in range(len(real)+1)] for i in range(len(fake)+1)]
+for i in range(len(fake)+1):
+    a[i][0] = i
+for j in range(len(real)+1):
+    a[0][j] = j
+for i in a:
+    print(i, end="\n")
