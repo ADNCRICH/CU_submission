eba69302b85d57b521aedda28d718f65a4b71478
ADNCRICH <p50629.2013x@gmail.com>
2021-12-11 15:08:03 +0700

    initial

---
+++ Python/PY/03/03_If_06.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+if n <= 100:
+    print(18)
+elif n <= 250:
+    print(22)
+elif n <= 500:
+    print(28)
+elif n <= 1000:
+    print(38)
+elif n <= 2000:
+    print(58)
+else:
+    print("Reject")
