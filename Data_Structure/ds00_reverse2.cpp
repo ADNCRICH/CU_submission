42faf304a1cbbc367c7e5ec875f188b4445ab958
ADNCRICH <p50629.2013x@gmail.com>
2022-08-19 14:32:50 +0700

    woowwww

---
+++ Data_Structure/ds00_reverse2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void reverse(vector<int> &v, vector<int>::iterator a, vector<int>::iterator b) {
+    // write your code only in this function
+    if (a > --b) swap(a, b);
+    while (a < b) swap(*(a++), *(b--));
+}
+int main() {
+    // read input
+    int n, a, b;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int c;
+        cin >> c;
+        v.push_back(c);
+    }
+    cin >> a >> b;
+    // call function
+    reverse(v, v.begin() + a, v.begin() + b + 1);
+    // display content of the vector
+    for (auto &x : v) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
--- Data_Structure/ds01_pair_sum.cpp
+++ Data_Structure/ds01_pair_sum.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 set<int> s;
-int a[];
+int a[1000100], dp[2000100];
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m;
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
+    int n, m, x, i, j;
     cin >> n >> m;
-    while (n--)
-        ;
+    for (i = 0; i < n; i++)
+        cin >> a[i];
+    for (i = 0; i < n; i++)
+        for (j = i + 1; j < n; j++)
+            if (a[i] + a[j] <= 2000000)
+                dp[a[i] + a[j]] = 1;
+    // for (i = 0; i < 20; i++)
+    //     cout << dp[i] << " ";
+    while (m--) {
+        cin >> x;
+        cout << (dp[x] ? "YES\n" : "NO\n");
+    }
 }
