7ee9cfa8aa3c679c4498042c0b92558ff0d17de0
ADNCRICH <p50629.2013x@gmail.com>
2022-09-24 02:14:32 +0700

    GOOD NIGHT

--- Data_Structure/own_data/d59_q1_range_insert/student.h
+++ Data_Structure/own_data/d59_q1_range_insert/student.h
@@ -1,10 +1,18 @@
 #ifndef __STUDENT_H_
 #define __STUDENT_H_
 
-
 template <typename T>
-void CP::vector<T>::insert(iterator position,iterator first,iterator last) {
-  //write your code here
+void CP::vector<T>::insert(iterator position, iterator first, iterator last) {
+    // write your code here
+    auto t = *this;
+    mSize = 0;
+    for (int i = 0; i <= t.size(); i++) {
+        if (begin() + i == position)
+            for (auto it = first; it < last; it++)
+                push_back(*it);
+        if (i < t.size())
+            push_back(t[i]);
+    }
 }
 
 #endif
--- Data_Structure/own_data/d59_q1_range_insert/vector.h
+++ Data_Structure/own_data/d59_q1_range_insert/vector.h
@@ -1,184 +1,177 @@
 #ifndef _CP_VECTOR_INCLUDED_
 #define _CP_VECTOR_INCLUDED_
 
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 //#pragma once
 
-namespace CP { 
+namespace CP {
 
 template <typename T>
-class vector
-{
-  public:
+class vector {
+   public:
     typedef T* iterator;
 
-  protected:
-    T *mData;
+   protected:
+    T* mData;
     size_t mCap;
     size_t mSize;
 
     void rangeCheck(int n) {
-      if (n < 0 || (size_t)n >= mSize) {
-        throw std::out_of_range("index of out range") ;
-      }
+        if (n < 0 || (size_t)n >= mSize) {
+            throw std::out_of_range("index of out range");
+        }
     }
 
     void expand(size_t capacity) {
-      T *arr = new T[capacity]();
-      for (size_t i = 0;i < mSize;i++) {
-        arr[i] = mData[i];
-      }
-      delete [] mData;
-      mData = arr;
-      mCap = capacity;
+        T* arr = new T[capacity]();
+        for (size_t i = 0; i < mSize; i++) {
+            arr[i] = mData[i];
+        }
+        delete[] mData;
+        mData = arr;
+        mCap = capacity;
     }
 
     void ensureCapacity(size_t capacity) {
-      if (capacity > mCap) {
-        size_t s = (capacity > 2 * mCap) ? capacity : 2 * mCap;
-        expand(s);
-      }
+        if (capacity > mCap) {
+            size_t s = (capacity > 2 * mCap) ? capacity : 2 * mCap;
+            expand(s);
+        }
     }
 
-  public:
+   public:
     //-------------- constructor & copy operator ----------
 
     // copy constructor
     vector(const vector<T>& a) {
-      mData = new T[a.capacity()]();
-      mCap = a.capacity();
-      mSize = a.size();
-      for (size_t i = 0;i < a.size();i++) {
-        mData[i] = a[i];
-      }
+        mData = new T[a.capacity()]();
+        mCap = a.capacity();
+        mSize = a.size();
+        for (size_t i = 0; i < a.size(); i++) {
+            mData[i] = a[i];
+        }
     }
 
     // default constructor
     vector() {
-      int cap = 1;
-      mData = new T[cap]();
-      mCap = cap;
-      mSize = 0;
+        int cap = 1;
+        mData = new T[cap]();
+        mCap = cap;
+        mSize = 0;
     }
 
     // constructor with initial size
     vector(size_t cap) {
-      mData = new T[cap]();
-      mCap = cap;
-      mSize = cap;
+        mData = new T[cap]();
+        mCap = cap;
+        mSize = cap;
     }
 
     // copy assignment operator using copy-and-swap idiom
     vector<T>& operator=(vector<T> other) {
-      // other is copy-constructed which will be destruct at the end of this scope
-      // we swap the content of this class to the other class and let it be descructed
-      using std::swap;
-      swap(this->mSize, other.mSize);
-      swap(this->mCap, other.mCap);
-      swap(this->mData, other.mData);
-      return *this;
+        // other is copy-constructed which will be destruct at the end of this scope
+        // we swap the content of this class to the other class and let it be descructed
+        using std::swap;
+        swap(this->mSize, other.mSize);
+        swap(this->mCap, other.mCap);
+        swap(this->mData, other.mData);
+        return *this;
     }
 
     ~vector() {
-      delete [] mData;
+        delete[] mData;
     }
 
     //------------- capacity function -------------------
     bool empty() const {
-      return mSize == 0;
+        return mSize == 0;
     }
 
     size_t size() const {
-      return mSize;
+        return mSize;
     }
 
     size_t capacity() const {
-      return mCap;
+        return mCap;
     }
 
     void resize(size_t n) {
-      if (n > mCap)
-        expand(n);
+        if (n > mCap)
+            expand(n);
 
-      if (n > mSize) {
-        T init = T();
-        for (size_t i = mSize;i < n;i++) 
-          mData[i] = init;
-      }
+        if (n > mSize) {
+            T init = T();
+            for (size_t i = mSize; i < n; i++)
+                mData[i] = init;
+        }
 
-
-      mSize = n;
+        mSize = n;
     }
 
     //----------------- iterator ---------------
     iterator begin() {
-      return &mData[0];
+        return &mData[0];
     }
 
     iterator end() {
-      return begin()+mSize;
-
+        return begin() + mSize;
     }
     //----------------- access -----------------
     T& at(int index) {
-      rangeCheck(index);
-      return mData[index];
+        rangeCheck(index);
+        return mData[index];
     }
 
     T& at(int index) const {
-      rangeCheck(index);
-      return mData[index];
+        rangeCheck(index);
+        return mData[index];
     }
 
     T& operator[](int index) {
-      return mData[index];
+        return mData[index];
     }
 
     T& operator[](int index) const {
-      return mData[index];
+        return mData[index];
     }
 
     //----------------- modifier -------------
     void push_back(const T& element) {
-      insert(end(),element);
+        insert(end(), element);
     }
 
     void pop_back() {
-      mSize--;
+        mSize--;
     }
 
-    iterator insert(iterator it,const T& element) {
-      size_t pos = it - begin();
-      ensureCapacity(mSize + 1);
-      for(size_t i = mSize;i > pos;i--) {
-        mData[i] = mData[i-1];
-      }
-      mData[pos] = element;
-      mSize++;
-      return begin()+pos;
+    iterator insert(iterator it, const T& element) {
+        size_t pos = it - begin();
+        ensureCapacity(mSize + 1);
+        for (size_t i = mSize; i > pos; i--) {
+            mData[i] = mData[i - 1];
+        }
+        mData[pos] = element;
+        mSize++;
+        return begin() + pos;
     }
 
     void erase(iterator it) {
-      while((it+1)!=end()) {
-        *it = *(it+1);
-        it++;
-      }
-      mSize--;
+        while ((it + 1) != end()) {
+            *it = *(it + 1);
+            it++;
+        }
+        mSize--;
     }
 
     void clear() {
-      mSize = 0;
+        mSize = 0;
     }
 
-
-    void insert(iterator position,iterator first,iterator last);
-
+    void insert(iterator position, iterator first, iterator last);
 };
 
-}
+}  // namespace CP
 
 #endif
-
-
