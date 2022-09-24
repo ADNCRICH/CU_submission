8b21ed668750b4c8b9fe36fe86e749e73e4dcf87
ADNCRICH <p50629.2013x@gmail.com>
2022-09-25 01:06:39 +0700

    Goo

--- Data_Structure/own_data/d59_q1_pop_many/student.h
+++ Data_Structure/own_data/d59_q1_pop_many/student.h
@@ -1,17 +1,23 @@
 #ifndef __STUDENT_H_
 #define __STUDENT_H_
 
-
 template <typename T>
 void CP::stack<T>::multi_pop(size_t K) {
-  //write your code here
+    // write your code here
+    mSize -= std::min(mSize, K);
 }
 
 template <typename T>
 std::stack<T> CP::stack<T>::remove_top(size_t K) {
-  //write your code here
-  //
-  //don't forget to return an std::stack
+    // write your code here
+    //
+    // don't forget to return an std::stack
+    K = std::min(K, mSize);
+    std::stack<T> t;
+    for (size_t i = 0; i < K; i++)
+        t.push(mData[mSize - K + i]);
+    multi_pop(K);
+    return t;
 }
 
 #endif
