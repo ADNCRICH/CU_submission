3ed87ee98e2dec7ad80b424591319196fa6989d9
ADNCRICH <p50629.2013x@gmail.com>
2023-05-20 01:02:53 +0700

    NICEE GOOD JOB BROO

---
+++ Project/Embedded_Final_Water/index.js
@@ -0,0 +1,127 @@
+import { initializeApp } from "firebase/app";
+//FireStore
+import { getFirestore, collection, getDocs, doc, addDoc } from "firebase/firestore";
+//RTOS
+import { getDatabase, set, ref, get } from "firebase/database";
+//express setup
+import express from "express";
+import bodyParser from "body-parser";
+import { firebaseConfig } from "./firebaseConfig.js";
+
+var appE = express();
+appE.use(bodyParser.json());
+appE.use(bodyParser.urlencoded({ extended: true }));
+var server = appE.listen(3000, console.log("Server is running on port 3000"));
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const dbRTOS = getDatabase(app);
+
+//get firestore
+appE.get("/api/getFIRE", async (req, res) => {
+    try {
+        const waterCol = collection(db, "Water History");
+        const waterSnap = await getDocs(waterCol);
+        const waterList = waterSnap.docs.map((doc) => [doc.id, doc.data()]);
+        return res.status(200).json({
+            RespCode: 200,
+            RespMessage: "Found, Nice my buddy.",
+            Result: waterList,
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            RespCode: 500,
+            RespMessage: err.message + " in get's catch ",
+        });
+    }
+});
+
+appE.post("/api/createFIRE", async (req, res) => {
+    var DissolvedO2 = req.body.DissolvedO2_Value;
+    var TDS = req.body.TDS_Value;
+    var Thermo = req.body.Thermo_Value;
+    console.log(DissolvedO2, TDS, Thermo);
+    try {
+        await addDoc(collection(db, "Water History"), {
+            DissolvedO2_Value: DissolvedO2,
+            TDS_Value: TDS,
+            Thermo_Value: Thermo,
+        });
+
+        return res.status(200).json({
+            RespCode: 200,
+            RespMessage: "Good job my guy :)",
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            RespCode: 500,
+            RespMessage: err.message + " in post's catch ",
+        });
+    }
+});
+
+//get RTOS and push to FireStore
+appE.get("/api/getRTOS", async (req, res) => {
+    try {
+        get(ref(dbRTOS))
+            .then(async (snap) => {
+                if (snap.exists()) {
+                    console.log(snap.val());
+                    try {
+                        await fetch("http://127.0.0.1:3000/api/createFIRE", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                DissolvedO2_Value: snap.val().DissolvedO2_Value,
+                                TDS_Value: snap.val().TDS_Value,
+                                Thermo_Value: snap.val().Thermo_Value,
+                            }),
+                        });
+                        console.log("GET from RTOS and Added to FireBase");
+                    } catch (err) {
+                        console.log(err);
+                        return res.status(500).json({
+                            RespCode: 500,
+                            RespMessage: err.message + " in getRTOS -> create's catch ",
+                        });
+                    }
+                    return res.status(200).json({
+                        RespCode: 200,
+                        RespMessage: "Found, Nice my buddy.(Maybe Posting)",
+                        Result: snap.val(),
+                    });
+                } else {
+                    return res.status(200).json({
+                        RespCode: 200,
+                        RespMessage: "I try my best to find :(",
+                        Result: "Not found",
+                    });
+                }
+            })
+            .catch((err2) => {
+                console.log(err2);
+                return res.status(500).json({
+                    RespCode: 500,
+                    RespMessage: err2.message + " in get's call back catch",
+                });
+            });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            RespCode: 500,
+            RespMessage: err.message + " in get's catch ",
+        });
+    }
+});
+
+// >>>>> debugger for get document from FireStore <<<<<
+// async function getTest() {
+//     let res = await fetch("http://127.0.0.1:3000/api/getFire");
+//     let data = await res.json();
+//     return data;
+// }
+// getTest().then((x) => console.log(x.Result));
