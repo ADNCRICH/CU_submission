e390f997e50c4f2b7b75c9ef1c532f3ac6cd4150
ADNCRICH <p50629.2013x@gmail.com>
2023-06-02 00:21:32 +0700

    UPPPP

--- ML/Summer#2023/ML_Classification.py
+++ ML/Summer#2023/ML_Classification.py
@@ -1,6 +1,9 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn import tree
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from matplotlib import pyplot as plt
 
 d = []
 for i in [2, 9, 46, 47, 54, 81, 87, 97]:
@@ -32,13 +35,27 @@ X = X.reshape(X.shape[0], -1)
 
 print(X.shape, y.shape)
 # split data
-print("Splitting data...")
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+N = 5
+# model = LogisticRegression(random_state=0, max_iter=50000)
+model = tree.DecisionTreeClassifier()
+for i in range(N):
+    print("round", i+1)
 
-# train model
-print("Training...")
-clf = LogisticRegression(random_state=0).fit(X_train, y_train)
+    # split data
+    print("Splitting data...")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# test model
-print("Testing...")
-print(clf.score(X_test, y_test))
+    # train model
+    print("Training...")
+    clf = model.fit(X_train, y_train)
+
+    # test model
+    print("Testing...")
+    print(clf.score(X_test, y_test))
+
+    y_pred = clf.predict(X_test)
+    CM = confusion_matrix(y_test, y_pred, labels=clf.classes_)
+    disp = ConfusionMatrixDisplay(CM, display_labels=clf.classes_)
+    disp.plot()
+    disp.ax_.set_title("round %d" % (i+1))
+plt.show()
