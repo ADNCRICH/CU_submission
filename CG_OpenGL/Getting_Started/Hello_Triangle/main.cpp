aea97f98359c54f033913a6418b8194f09f13069
ADNCRICH <p50629.2013x@gmail.com>
2024-08-14 11:39:56 +0700

    TRIANGLE

--- CG_OpenGL/Getting_Started/Hello_Triangle/main.cpp
+++ CG_OpenGL/Getting_Started/Hello_Triangle/main.cpp
@@ -55,21 +55,26 @@ int main(){
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    GLuint VBO;
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO); 
     glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glUseProgram(shaderProgram);
-
-
+    
     while(!glfwWindowShouldClose(window)){
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
