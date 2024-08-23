07e4bb224402279c5221a6c1001310d2550b4bc3
ADNCRICH <p50629.2013x@gmail.com>
2024-08-23 15:13:37 +0700

    IMG

--- CG_OpenGL/Getting_Started/05_Textures/main.cpp
+++ CG_OpenGL/Getting_Started/05_Textures/main.cpp
@@ -168,7 +168,7 @@ int main()
     int width, height, nrChannels;
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
     stbi_set_flip_vertically_on_load(true);
-    unsigned char *data = stbi_load((std::filesystem::current_path().string() + "/resource/image_5.png").c_str(), &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load((std::filesystem::current_path().string() + "/resource/image_2.png").c_str(), &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
