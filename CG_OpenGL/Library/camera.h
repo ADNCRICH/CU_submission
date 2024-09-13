f6ca0c850f4a3330894dcf4159a709f876c23179
ADNCRICH <p50629.2013x@gmail.com>
2024-09-13 11:56:18 +0700

    LIGHT

--- CG_OpenGL/Library/camera.h
+++ CG_OpenGL/Library/camera.h
@@ -17,7 +17,7 @@ enum Camera_Movement {
 const float YAW         = -90.0f;
 const float PITCH       =  0.0f;
 const float SPEED       =  2.5f;
-const float SENSITIVITY =  0.1f;
+const float SENSITIVITY =  0.001f;
 const float ZOOM        =  45.0f;
 
 
